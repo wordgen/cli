92ea15f5367624fbf926d13ca6cbdafac62d84ea
Daniel Kuehn <daniel@kuehn.foo>
2024-06-13 12:46:44 -0400

    refactor(scripts/update-aur): add support for -bin package

--- scripts/update-aur.sh
+++ scripts/update-aur.sh
@@ -2,36 +2,39 @@
 
 set -e
 
-PKGBUILD="$HOME/git/PKGBUILDS/wordgen/PKGBUILD"
-REPO_URL="https://github.com/wordgen/cli"
+PKGBUILDS=("$HOME/git/PKGBUILDS/wordgen/PKGBUILD"
+           "$HOME/git/PKGBUILDS/wordgen-bin/PKGBUILD")
 VERSION="$(grep -E '^VERSION[[:space:]]*:=[[:space:]]*' Makefile | awk '{print $3}')"
+REPO_URL="https://github.com/wordgen/cli"
 TEMP_DIR="$(mktemp -d)"
 
-trap 'rm -rf $TEMP_DIR' EXIT INT TERM
+trap 'rm -rf "$TEMP_DIR"' EXIT INT TERM
 
-cp "$PKGBUILD" "$TEMP_DIR"
-cd "$TEMP_DIR"
+for pkgbuild in "${PKGBUILDS[@]}"; do
+	cp "$pkgbuild" "$TEMP_DIR"
+	cd "$TEMP_DIR"
 
-sed -i "s/^pkgver=.*/pkgver=${VERSION#v}/" PKGBUILD
-sed -i "s/^pkgrel=.*/pkgrel=1/" PKGBUILD
+	sed -i "s/^pkgver=.*/pkgver=${VERSION#v}/" PKGBUILD
+	sed -i "s/^pkgrel=.*/pkgrel=1/" PKGBUILD
 
-if ! updpkgsums; then
-	echo "ERROR: updpkgsums failed"
-	exit 1
-fi
+	if ! updpkgsums; then
+		echo "ERROR: updpkgsums failed for $pkgbuild"
+		exit 1
+	fi
 
-rm -f ./*.tar.gz
+	find . -maxdepth 1 -mindepth 1 ! -name 'PKGBUILD' -exec rm -rf {} +
 
-if ! makepkg --clean; then
-	echo "ERROR: makepkg --clean failed"
-	exit 1
-fi
+	if ! makepkg --clean; then
+		echo "ERROR: makepkg --clean failed for $pkgbuild"
+		exit 1
+	fi
 
-mv PKGBUILD "$PKGBUILD"
-cd "$(dirname "$PKGBUILD")"
+	mv PKGBUILD "$pkgbuild"
+	cd "$(dirname "$pkgbuild")"
 
-makepkg --printsrcinfo > .SRCINFO
+	makepkg --printsrcinfo > .SRCINFO
 
-git add PKGBUILD .SRCINFO
-git commit -m "$VERSION" -m "- $REPO_URL/releases/tag/$VERSION"
-git push
+	git add PKGBUILD .SRCINFO
+	git commit -m "$VERSION" -m "- $REPO_URL/releases/tag/$VERSION"
+	git push
+done
