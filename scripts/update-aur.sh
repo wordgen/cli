61b456b5ae4b327909c06ac045d2a330f076c54e
Daniel Kuehn <daniel@kuehn.foo>
2024-06-16 23:51:28 -0400

    scripts(update-aur): refactor for better isolation and cleanup

--- scripts/update-aur.sh
+++ scripts/update-aur.sh
@@ -4,37 +4,45 @@ set -e
 
 PKGBUILDS=("$HOME/git/PKGBUILDS/wordgen/PKGBUILD"
            "$HOME/git/PKGBUILDS/wordgen-bin/PKGBUILD")
-VERSION="$(grep -E '^VERSION[[:space:]]*:=[[:space:]]*' Makefile | awk '{print $3}')"
+VERSION="$(grep -E '^VERSION[[:space:]]*:=[[:space:]]*' ./Makefile | awk '{print $3}')"
 REPO_URL="https://github.com/wordgen/cli"
 TEMP_DIR="$(mktemp -d)"
 
 trap 'rm -rf "$TEMP_DIR"' EXIT INT TERM
 
-for pkgbuild in "${PKGBUILDS[@]}"; do
-	cp "$pkgbuild" "$TEMP_DIR"
-	cd "$TEMP_DIR"
+update_pkgbuild() {
+	local pkgbuild="$1" temp_dir
 
-	sed -i "s/^pkgver=.*/pkgver=${VERSION#v}/" PKGBUILD
-	sed -i "s/^pkgrel=.*/pkgrel=1/" PKGBUILD
+	temp_dir="$(mktemp -d --tmpdir="$TEMP_DIR")"
+
+	cp "$pkgbuild" "$temp_dir"
+	cd "$temp_dir"
+
+	sed -i "s/^pkgver=.*/pkgver=${VERSION#v}/" ./PKGBUILD
+	sed -i "s/^pkgrel=.*/pkgrel=1/" ./PKGBUILD
 
 	if ! updpkgsums; then
 		echo "ERROR: updpkgsums failed for $pkgbuild"
 		exit 1
 	fi
 
-	find . -maxdepth 1 -mindepth 1 ! -name 'PKGBUILD' -exec rm -rf {} +
+	rm -f ./*.tar.gz
 
 	if ! makepkg --clean; then
 		echo "ERROR: makepkg --clean failed for $pkgbuild"
 		exit 1
 	fi
 
-	mv PKGBUILD "$pkgbuild"
+	mv ./PKGBUILD "$pkgbuild"
 	cd "$(dirname "$pkgbuild")"
 
-	makepkg --printsrcinfo > .SRCINFO
+	makepkg --printsrcinfo > ./.SRCINFO
 
-	git add PKGBUILD .SRCINFO
+	git add ./PKGBUILD ./.SRCINFO
 	git commit -m "$VERSION" -m "- $REPO_URL/releases/tag/$VERSION"
 	git push
+}
+
+for pkgbuild in "${PKGBUILDS[@]}"; do
+	update_pkgbuild "$pkgbuild"
 done
